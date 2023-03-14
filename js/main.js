7bc8f6d4d408e921f92194a6581a74a4902aa714
Kulzhebaev Azamat <124907601+AKulzhebaev@users.noreply.github.com>
2023-03-14 21:11:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnLogin = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".icon-close");
+
+
+
+registerLink.addEventListener("click", function(){
+   wrapper.classList.add("active");
+})
+
+loginLink.addEventListener("click", function(){
+   wrapper.classList.remove("active");
+})
+
+btnLogin.addEventListener("click", function(){
+   wrapper.classList.add("active-popup");
+})
+
+
+iconClose.addEventListener("click", function(){
+   wrapper.classList.remove("active-popup");
+})
